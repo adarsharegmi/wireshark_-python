b0626631ebe370062abf5714e4fa97c0963509ed
AddyR <adarsharegmi121@gmail.com>
2022-05-31 23:23:07 +0545

    data will be send interface name

--- server.py
+++ server.py
@@ -7,30 +7,49 @@ import asyncio
 
 app = Sanic("Streaming_dataset")
 
+def call_data(interface=r'eth'):
+    capture = pyshark.LiveCapture(interface=interface)
+    capture.sniff(packet_count=2)
+    listsd = []
+    for packet in capture.sniff_continuously(packet_count=0):
+        # print ('Just arrived:', packet)
+        di = {}
+        captured_length = packet.captured_length
+        attributes= [f for  f  in dir(packet) if '__'  not in f]
+        for i in attributes:
+            var = getattr(packet,i)
+            if isinstance(var, str):
+                di.update({i: var})
+            else:
+                attributes= [f for  f  in dir(var) if '__'  not in f]
+                for i in attributes:
+                    var2 = getattr(var,i)
+                    if isinstance(var2, str):
+                        di.update({i: var2})
+            
+            listsd.append(di)
+            
+        return str(listsd)
 
-def call_data():
-    capture = pyshark.LiveCapture(interface=r'Wi-Fi')
-    data = capture.sniff(packet_count=0)
-    breakpoint()
-    return data
 
-
-async def get_data():
+async def get_data(interface):
         loop = asyncio.get_event_loop()
-        result = await loop.run_in_executor(None, call_data)
+        result = await loop.run_in_executor(None, call_data,interface)
+        return result
 
 @app.websocket('/feed')
 async def feed(request, ws):
     while True:
         data="hello ! The current date is" +  str(datetime.datetime.now())
-        data = await get_data()
+        interface =await ws.recv()
+        
+        data = await get_data(interface)
         if data:
-            print(str(data))
             await ws.send(str(data))
         else:
             await ws.send("None received")
-        data=await ws.recv()
-        print("received"+data)
+
+        #print("received"+data)
 
 
 
